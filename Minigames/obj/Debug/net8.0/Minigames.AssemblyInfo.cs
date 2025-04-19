b5f9710bb6ec3f68cd958eddf3ecc74fe02ae6a5
Antoine LEGOIS <antoine.legois@altimet.fr>
2025-04-19 15:25:58 +0200

    home page addedd;

--- Minigames/obj/Debug/net8.0/Minigames.AssemblyInfo.cs
+++ Minigames/obj/Debug/net8.0/Minigames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minigames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2567ef92c58343cb16b29f4ba8487172c46e44d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d03cfc1f44e04a64d9c8ddd884476d78be07f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minigames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minigames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
